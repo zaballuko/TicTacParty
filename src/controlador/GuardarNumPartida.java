29beb3a410917f0b5b707ae727d3241ba9451f10
davidZabal <zaballuko@gmail.com>
2018-04-25 12:32:12 +0200

    GuardaPartida terminado

--- src/controlador/GuardarNumPartida.java
+++ src/controlador/GuardarNumPartida.java
@@ -1,6 +1,7 @@
 package controlador;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 public class GuardarNumPartida extends HttpServlet{
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		
+		String numPartidas = request.getParameter("numPartidas");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.print(numPartidas);
 	}
 }
