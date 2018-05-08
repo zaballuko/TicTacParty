9d67f830af42402a84e737b5d17ec24a0934c7c1
Iker Apaolaza <iapaolazacl17dw@ikzubirimanteo.com>
2018-05-08 11:15:47 +0200

    Añadidos fondos y creada página para crear nueva cuenta.

--- src/Modelo/Config.java
+++ src/Modelo/Config.java
@@ -4,7 +4,7 @@ public interface Config {
 	public static final String BBDD = "tictacparty";
 	public static final String HOST = "10.14.3.86";
 
-	public static final String USERNAME = "david";
+	public static final String USERNAME = "iker";
 	public static final String PASSWORD = "zubiri";
 
 }
